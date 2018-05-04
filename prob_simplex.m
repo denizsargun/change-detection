e7c3f34117d9b148a43ac607a7f46161ced8691a
Deniz Sargun <denizsargun@gmail.com>
2018-05-04 14:31:19 -0400

    Add files via upload

---
+++ prob_simplex.m
@@ -0,0 +1,18 @@
+classdef prob_simplex
+    %PROB_SIMPLEX define a simple probability simplex
+    %   alphabet is fixed
+    
+    properties
+        n
+        a = [-1;0;1]
+    end
+    
+    methods
+        function g = see_simplex()
+            r = round([obj.Value],2);
+        end
+        
+    end
+    
+end
+
