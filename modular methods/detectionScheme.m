9c97df1d79e394cae1d5a4bc589d447dcd18c79b
Deniz Sargun <denizsargun@gmail.com>
2018-10-29 18:58:35 -0400

    included mtbf+further testing

---
+++ modular methods/detectionScheme.m
@@ -0,0 +1,20 @@
+classdef detectionScheme < handle
+    properties
+        scenario
+        parameters
+        pfaTest
+        pmdTest
+        wcddTest
+        mtbfTest
+    end
+    
+    methods
+        function obj = detectionScheme()
+        end
+        
+    end
+    
+    methods(Static)
+    end
+    
+end
---
+++ modular methods/monteCarlo.m
@@ -0,0 +1,17 @@
+classdef monteCarlo < handle
+    properties
+        scenario
+        parameters
+        klScheme
+        meanScheme
+        lmpScheme
+        glrScheme
+    end
+    
+    methods
+    end
+    
+    methods(Static)
+    end
+    
+end
