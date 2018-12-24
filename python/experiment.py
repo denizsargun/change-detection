e977a540caff879f56636260c9254d91e9edf4d2
Sargun <sargun.1@coeit.osu.edu>
2018-12-23 21:41:18 -0500

    Update experiment.py

--- python/experiment.py
+++ python/experiment.py
@@ -1,9 +1,42 @@
 class experiment(object):
 
     def __init__(self):
+        activeTestIndex
+        alpha
         alphabet
+        alphabetSize
         beta
-        initDist
-        utility
+        changedDist
+        cvxFolder
+        cvxPrecision
+        cvxSetupFile
+        eps
+        storageFile
+        glrThrRange
+        gmaDist
+        iProj
+        klMeanRange
+        klRadiusRange
+        maxFunEvals
+        maxIter
+        lmpDir
+        lmpThrRange
+        meanMeanRange
+        mProj
+        mtbf
+        numberOfReps
+        performance
+        performanceMean
+        performanceStd
+        pfaIt
+        pmdIt
+        sampleSize
+        stringLength
+        testNames
+        testTime % tic-toc holder
+        testTypes
+        theta % learning rate of empirical dist
+        unchangedDist
+        utility % utility toolbox
 
     def 
