1b4ed0324ad88cac1496a817fcd6f2bd43659fa5
denizsargun <denizsargun@gmail.com>
2019-02-04 11:40:40 -0500

    new exps

--- optimum kl/changing alphabet size/experiments/plotAll.m
+++ optimum kl/changing alphabet size/experiments/plotAll.m
@@ -3,109 +3,327 @@ klM_timeT11 = xlsread(excelFile,'klM_timeT');
 meanM_timeT11 = xlsread(excelFile,'meanM_timeT');
 lmpM_timeT11 = xlsread(excelFile,'lmpM_timeT');
 glrM_timeT11 = xlsread(excelFile,'glrM_timeT');
+
 excelFile = 'experiment_N1M2_2019_02_03_17_52_25.xls';
 klM_timeT12 = xlsread(excelFile,'klM_timeT');
 meanM_timeT12 = xlsread(excelFile,'meanM_timeT');
 lmpM_timeT12 = xlsread(excelFile,'lmpM_timeT');
 glrM_timeT12 = xlsread(excelFile,'glrM_timeT');
+
 excelFile = 'experiment_N1M4_2019_02_03_17_52_46.xls';
 klM_timeT14 = xlsread(excelFile,'klM_timeT');
 meanM_timeT14 = xlsread(excelFile,'meanM_timeT');
 lmpM_timeT14 = xlsread(excelFile,'lmpM_timeT');
 glrM_timeT14 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N1M8_2019_02_03_19_42_11.xls';
+klM_timeT18 = xlsread(excelFile,'klM_timeT');
+meanM_timeT18 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT18 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT18 = xlsread(excelFile,'glrM_timeT');
+
+
 excelFile = 'experiment_N2M1_2019_02_03_17_53_08.xls';
 klM_timeT21 = xlsread(excelFile,'klM_timeT');
 meanM_timeT21 = xlsread(excelFile,'meanM_timeT');
 lmpM_timeT21 = xlsread(excelFile,'lmpM_timeT');
 glrM_timeT21 = xlsread(excelFile,'glrM_timeT');
+
 excelFile = 'experiment_N2M2_2019_02_03_17_54_41.xls';
 klM_timeT22 = xlsread(excelFile,'klM_timeT');
 meanM_timeT22 = xlsread(excelFile,'meanM_timeT');
 lmpM_timeT22 = xlsread(excelFile,'lmpM_timeT');
 glrM_timeT22 = xlsread(excelFile,'glrM_timeT');
+
 excelFile = 'experiment_N2M4_2019_02_03_17_56_28.xls';
 klM_timeT24 = xlsread(excelFile,'klM_timeT');
 meanM_timeT24 = xlsread(excelFile,'meanM_timeT');
 lmpM_timeT24 = xlsread(excelFile,'lmpM_timeT');
 glrM_timeT24 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N2M8_2019_02_03_19_42_37.xls';
+klM_timeT28 = xlsread(excelFile,'klM_timeT');
+meanM_timeT28 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT28 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT28 = xlsread(excelFile,'glrM_timeT');
+
+
 excelFile = 'experiment_N3M1_2019_02_03_17_58_29.xls';
 klM_timeT31 = xlsread(excelFile,'klM_timeT');
 meanM_timeT31 = xlsread(excelFile,'meanM_timeT');
 lmpM_timeT31 = xlsread(excelFile,'lmpM_timeT');
 glrM_timeT31 = xlsread(excelFile,'glrM_timeT');
+
 excelFile = 'experiment_N3M2_2019_02_03_18_01_56.xls';
 klM_timeT32 = xlsread(excelFile,'klM_timeT');
 meanM_timeT32 = xlsread(excelFile,'meanM_timeT');
 lmpM_timeT32 = xlsread(excelFile,'lmpM_timeT');
 glrM_timeT32 = xlsread(excelFile,'glrM_timeT');
+
 excelFile = 'experiment_N3M4_2019_02_03_18_05_17.xls';
 klM_timeT34 = xlsread(excelFile,'klM_timeT');
 meanM_timeT34 = xlsread(excelFile,'meanM_timeT');
 lmpM_timeT34 = xlsread(excelFile,'lmpM_timeT');
 glrM_timeT34 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N3M8_2019_02_03_19_44_55.xls';
+klM_timeT38 = xlsread(excelFile,'klM_timeT');
+meanM_timeT38 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT38 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT38 = xlsread(excelFile,'glrM_timeT');
+
+
 excelFile = 'experiment_N4M1_2019_02_03_18_08_40.xls';
 klM_timeT41 = xlsread(excelFile,'klM_timeT');
 meanM_timeT41 = xlsread(excelFile,'meanM_timeT');
 lmpM_timeT41 = xlsread(excelFile,'lmpM_timeT');
 glrM_timeT41 = xlsread(excelFile,'glrM_timeT');
+
 excelFile = 'experiment_N4M2_2019_02_03_18_31_40.xls';
 klM_timeT42 = xlsread(excelFile,'klM_timeT');
 meanM_timeT42 = xlsread(excelFile,'meanM_timeT');
 lmpM_timeT42 = xlsread(excelFile,'lmpM_timeT');
 glrM_timeT42 = xlsread(excelFile,'glrM_timeT');
+
 excelFile = 'experiment_N4M4_2019_02_03_18_40_52.xls';
 klM_timeT44 = xlsread(excelFile,'klM_timeT');
 meanM_timeT44 = xlsread(excelFile,'meanM_timeT');
 lmpM_timeT44 = xlsread(excelFile,'lmpM_timeT');
 glrM_timeT44 = xlsread(excelFile,'glrM_timeT');
 
-klM_time1 = [klM_timeT11; klM_timeT12; klM_timeT14];
-klM_time2 = [klM_timeT21; klM_timeT22; klM_timeT24];
-klM_time3 = [klM_timeT31; klM_timeT32; klM_timeT34];
-klM_time4 = [klM_timeT41; klM_timeT42; klM_timeT44];
-meanM_time1 = [meanM_timeT11; meanM_timeT12; meanM_timeT14];
-meanM_time2 = [meanM_timeT21; meanM_timeT22; meanM_timeT24];
-meanM_time3 = [meanM_timeT31; meanM_timeT32; meanM_timeT34];
-meanM_time4 = [meanM_timeT41; meanM_timeT42; meanM_timeT44];
-lmpM_time1 = [lmpM_timeT11; lmpM_timeT12; lmpM_timeT14];
-lmpM_time2 = [lmpM_timeT21; lmpM_timeT22; lmpM_timeT24];
-lmpM_time3 = [lmpM_timeT31; lmpM_timeT32; lmpM_timeT34];
-lmpM_time4 = [lmpM_timeT41; lmpM_timeT42; lmpM_timeT44];
-glrM_time1 = [glrM_timeT11; glrM_timeT12; glrM_timeT14];
-glrM_time2 = [glrM_timeT21; glrM_timeT22; glrM_timeT24];
-glrM_time3 = [glrM_timeT31; glrM_timeT32; glrM_timeT34];
-glrM_time4 = [glrM_timeT41; glrM_timeT42; glrM_timeT44];
+excelFile = 'experiment_N4M8_2019_02_03_19_48_43.xls';
+klM_timeT48 = xlsread(excelFile,'klM_timeT');
+meanM_timeT48 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT48 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT48 = xlsread(excelFile,'glrM_timeT');
+
+
+excelFile = 'experiment_N5M1_2019_02_03_19_58_03.xls';
+klM_timeT51 = xlsread(excelFile,'klM_timeT');
+meanM_timeT51 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT51 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT51 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N5M2_2019_02_03_20_18_07.xls';
+klM_timeT52 = xlsread(excelFile,'klM_timeT');
+meanM_timeT52 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT52 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT52 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N5M4_2019_02_03_20_39_32.xls';
+klM_timeT54 = xlsread(excelFile,'klM_timeT');
+meanM_timeT54 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT54 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT54 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N5M8_2019_02_03_21_01_56.xls';
+klM_timeT58 = xlsread(excelFile,'klM_timeT');
+meanM_timeT58 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT58 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT58 = xlsread(excelFile,'glrM_timeT');
+
+
+excelFile = 'experiment_N6M1_2019_02_03_21_28_07.xls';
+klM_timeT61 = xlsread(excelFile,'klM_timeT');
+meanM_timeT61 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT61 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT61 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N6M2_2019_02_03_21_52_35.xls';
+klM_timeT62 = xlsread(excelFile,'klM_timeT');
+meanM_timeT62 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT62 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT62 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N6M4_2019_02_03_22_13_01.xls';
+klM_timeT64 = xlsread(excelFile,'klM_timeT');
+meanM_timeT64 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT64 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT64 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N6M8_2019_02_03_22_30_30.xls';
+klM_timeT68 = xlsread(excelFile,'klM_timeT');
+meanM_timeT68 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT68 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT68 = xlsread(excelFile,'glrM_timeT');
+
+
+excelFile = 'experiment_N7M1_2019_02_03_22_47_11.xls';
+klM_timeT71 = xlsread(excelFile,'klM_timeT');
+meanM_timeT71 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT71 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT71 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N7M2_2019_02_03_22_59_51.xls';
+klM_timeT72 = xlsread(excelFile,'klM_timeT');
+meanM_timeT72 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT72 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT72 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N7M4_2019_02_03_23_12_52.xls';
+klM_timeT74 = xlsread(excelFile,'klM_timeT');
+meanM_timeT74 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT74 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT74 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N7M8_2019_02_03_23_25_48.xls';
+klM_timeT78 = xlsread(excelFile,'klM_timeT');
+meanM_timeT78 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT78 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT78 = xlsread(excelFile,'glrM_timeT');
+
+
+excelFile = 'experiment_N8M1_2019_02_03_23_39_05.xls';
+klM_timeT81 = xlsread(excelFile,'klM_timeT');
+meanM_timeT81 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT81 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT81 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N8M2_2019_02_03_23_56_01.xls';
+klM_timeT82 = xlsread(excelFile,'klM_timeT');
+meanM_timeT82 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT82 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT82 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N8M4_2019_02_04_00_12_50.xls';
+klM_timeT84 = xlsread(excelFile,'klM_timeT');
+meanM_timeT84 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT84 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT84 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N8M8_2019_02_04_00_30_11.xls';
+klM_timeT88 = xlsread(excelFile,'klM_timeT');
+meanM_timeT88 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT88 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT88 = xlsread(excelFile,'glrM_timeT');
+
+
+excelFile = 'experiment_N9M1_2019_02_04_00_47_58.xls';
+klM_timeT91 = xlsread(excelFile,'klM_timeT');
+meanM_timeT91 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT91 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT91 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N9M2_2019_02_04_01_18_32.xls';
+klM_timeT92 = xlsread(excelFile,'klM_timeT');
+meanM_timeT92 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT92 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT92 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N9M4_2019_02_04_01_49_47.xls';
+klM_timeT94 = xlsread(excelFile,'klM_timeT');
+meanM_timeT94 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT94 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT94 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N9M8_2019_02_04_02_25_01.xls';
+klM_timeT98 = xlsread(excelFile,'klM_timeT');
+meanM_timeT98 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT98 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT98 = xlsread(excelFile,'glrM_timeT');
+
+
+excelFile = 'experiment_N10M1_2019_02_04_03_00_19.xls';
+klM_timeT101 = xlsread(excelFile,'klM_timeT');
+meanM_timeT101 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT101 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT101 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N10M2_2019_02_04_03_37_46.xls';
+klM_timeT102 = xlsread(excelFile,'klM_timeT');
+meanM_timeT102 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT102 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT102 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N10M4_2019_02_04_04_18_42.xls';
+klM_timeT104 = xlsread(excelFile,'klM_timeT');
+meanM_timeT104 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT104 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT104 = xlsread(excelFile,'glrM_timeT');
+
+excelFile = 'experiment_N10M8_2019_02_04_04_59_04.xls';
+klM_timeT108 = xlsread(excelFile,'klM_timeT');
+meanM_timeT108 = xlsread(excelFile,'meanM_timeT');
+lmpM_timeT108 = xlsread(excelFile,'lmpM_timeT');
+glrM_timeT108 = xlsread(excelFile,'glrM_timeT');
+
+klM_time1 = [klM_timeT11; klM_timeT12; klM_timeT14; klM_timeT18];
+klM_time2 = [klM_timeT21; klM_timeT22; klM_timeT24; klM_timeT28];
+klM_time3 = [klM_timeT31; klM_timeT32; klM_timeT34; klM_timeT38];
+klM_time4 = [klM_timeT41; klM_timeT42; klM_timeT44; klM_timeT48];
+klM_time5 = [klM_timeT51; klM_timeT52; klM_timeT54; klM_timeT58];
+klM_time6 = [klM_timeT61; klM_timeT62; klM_timeT64; klM_timeT68];
+klM_time7 = [klM_timeT71; klM_timeT72; klM_timeT74; klM_timeT78];
+klM_time8 = [klM_timeT81; klM_timeT82; klM_timeT84; klM_timeT88];
+klM_time9 = [klM_timeT91; klM_timeT92; klM_timeT94; klM_timeT98];
+klM_time10 = [klM_timeT101; klM_timeT102; klM_timeT104; klM_timeT108];
+
+meanM_time1 = [meanM_timeT11; meanM_timeT12; meanM_timeT14; meanM_timeT18];
+meanM_time2 = [meanM_timeT21; meanM_timeT22; meanM_timeT24; meanM_timeT28];
+meanM_time3 = [meanM_timeT31; meanM_timeT32; meanM_timeT34; meanM_timeT38];
+meanM_time4 = [meanM_timeT41; meanM_timeT42; meanM_timeT44; meanM_timeT48];
+meanM_time5 = [meanM_timeT51; meanM_timeT52; meanM_timeT54; meanM_timeT58];
+meanM_time6 = [meanM_timeT61; meanM_timeT62; meanM_timeT64; meanM_timeT68];
+meanM_time7 = [meanM_timeT71; meanM_timeT72; meanM_timeT74; meanM_timeT78];
+meanM_time8 = [meanM_timeT81; meanM_timeT82; meanM_timeT84; meanM_timeT88];
+meanM_time9 = [meanM_timeT91; meanM_timeT92; meanM_timeT94; meanM_timeT98];
+meanM_time10 = [meanM_timeT101; meanM_timeT102; meanM_timeT104; meanM_timeT108];
+
+lmpM_time1 = [lmpM_timeT11; lmpM_timeT12; lmpM_timeT14; lmpM_timeT18];
+lmpM_time2 = [lmpM_timeT21; lmpM_timeT22; lmpM_timeT24; lmpM_timeT28];
+lmpM_time3 = [lmpM_timeT31; lmpM_timeT32; lmpM_timeT34; lmpM_timeT38];
+lmpM_time4 = [lmpM_timeT41; lmpM_timeT42; lmpM_timeT44; lmpM_timeT48];
+lmpM_time5 = [lmpM_timeT51; lmpM_timeT52; lmpM_timeT54; lmpM_timeT58];
+lmpM_time6 = [lmpM_timeT61; lmpM_timeT62; lmpM_timeT64; lmpM_timeT68];
+lmpM_time7 = [lmpM_timeT71; lmpM_timeT72; lmpM_timeT74; lmpM_timeT78];
+lmpM_time8 = [lmpM_timeT81; lmpM_timeT82; lmpM_timeT84; lmpM_timeT88];
+lmpM_time9 = [lmpM_timeT91; lmpM_timeT92; lmpM_timeT94; lmpM_timeT98];
+lmpM_time10 = [lmpM_timeT101; lmpM_timeT102; lmpM_timeT104; lmpM_timeT108];
+
+glrM_time1 = [glrM_timeT11; glrM_timeT12; glrM_timeT14; glrM_timeT18];
+glrM_time2 = [glrM_timeT21; glrM_timeT22; glrM_timeT24; glrM_timeT28];
+glrM_time3 = [glrM_timeT31; glrM_timeT32; glrM_timeT34; glrM_timeT38];
+glrM_time4 = [glrM_timeT41; glrM_timeT42; glrM_timeT44; glrM_timeT48];
+glrM_time5 = [glrM_timeT51; glrM_timeT52; glrM_timeT54; glrM_timeT58];
+glrM_time6 = [glrM_timeT61; glrM_timeT62; glrM_timeT64; glrM_timeT68];
+glrM_time7 = [glrM_timeT71; glrM_timeT72; glrM_timeT74; glrM_timeT78];
+glrM_time8 = [glrM_timeT81; glrM_timeT82; glrM_timeT84; glrM_timeT88];
+glrM_time9 = [glrM_timeT91; glrM_timeT92; glrM_timeT94; glrM_timeT98];
+glrM_time10 = [glrM_timeT101; glrM_timeT102; glrM_timeT104; glrM_timeT108];
 
 figure
 subplot(2,2,1)
 grid minor
 hold on
-plot([1 2 4], klM_time1(:,1),'black')
-plot([1 2 4], klM_time1(:,2),'black')
-plot([1 2 4], meanM_time1,'r')
-plot([1 2 4], lmpM_time1,'g')
-plot([1 2 4], glrM_time1,'b')
+plot(2.^(1:10), [klM_time1(1,1) klM_time2(1,1) klM_time3(1,1) klM_time4(1,1) klM_time5(1,1) klM_time6(1,1) klM_time7(1,1) klM_time8(1,1) klM_time9(1,1) klM_time10(1,1)],'black')
+plot(2.^(1:10), [klM_time1(1,2) klM_time2(1,2) klM_time3(1,2) klM_time4(1,2) klM_time5(1,2) klM_time6(1,2) klM_time7(1,2) klM_time8(1,2) klM_time9(1,2) klM_time10(1,2)],'black')
+plot(2.^(1:10), [meanM_time1(1) meanM_time2(1) meanM_time3(1) meanM_time4(1) meanM_time5(1) meanM_time6(1) meanM_time7(1) meanM_time8(1) meanM_time9(1) meanM_time10(1)],'r')
+plot(2.^(1:10), [lmpM_time1(1) lmpM_time2(1) lmpM_time3(1) lmpM_time4(1) lmpM_time5(1) lmpM_time6(1) lmpM_time7(1) lmpM_time8(1) lmpM_time9(1) lmpM_time10(1)],'g')
+plot(2.^(1:10), [glrM_time1(1) glrM_time2(1) glrM_time3(1) glrM_time4(1) glrM_time5(1) glrM_time6(1) glrM_time7(1) glrM_time8(1) glrM_time9(1) glrM_time10(1)],'b')
+
 subplot(2,2,2)
 grid minor
 hold on
-plot([1 2 4], klM_time2(:,1),'black')
-plot([1 2 4], klM_time2(:,2),'black')
-plot([1 2 4], meanM_time2,'r')
-plot([1 2 4], lmpM_time2,'g')
-plot([1 2 4], glrM_time2,'b')
+plot(2.^(1:10), [klM_time1(2,1) klM_time2(2,1) klM_time3(2,1) klM_time4(2,1) klM_time5(2,1) klM_time6(2,1) klM_time7(2,1) klM_time8(2,1) klM_time9(2,1) klM_time10(2,1)],'black')
+plot(2.^(1:10), [klM_time1(2,2) klM_time2(2,2) klM_time3(2,2) klM_time4(2,2) klM_time5(2,2) klM_time6(2,2) klM_time7(2,2) klM_time8(2,2) klM_time9(2,2) klM_time10(2,2)],'black')
+plot(2.^(1:10), [meanM_time1(2) meanM_time2(2) meanM_time3(2) meanM_time4(2) meanM_time5(2) meanM_time6(2) meanM_time7(2) meanM_time8(2) meanM_time9(2) meanM_time10(2)],'r')
+plot(2.^(1:10), [lmpM_time1(2) lmpM_time2(2) lmpM_time3(2) lmpM_time4(2) lmpM_time5(2) lmpM_time6(2) lmpM_time7(2) lmpM_time8(2) lmpM_time9(2) lmpM_time10(2)],'g')
+plot(2.^(1:10), [glrM_time1(2) glrM_time2(2) glrM_time3(2) glrM_time4(2) glrM_time5(2) glrM_time6(2) glrM_time7(2) glrM_time8(2) glrM_time9(2) glrM_time10(2)],'b')
+
 subplot(2,2,3)
 grid minor
 hold on
-plot([1 2 4], klM_time3(:,1),'black')
-plot([1 2 4], klM_time3(:,2),'black')
-plot([1 2 4], meanM_time3,'r')
-plot([1 2 4], lmpM_time3,'g')
-plot([1 2 4], glrM_time3,'b')
+plot(2.^(1:10), [klM_time1(3,1) klM_time2(3,1) klM_time3(3,1) klM_time4(3,1) klM_time5(3,1) klM_time6(3,1) klM_time7(3,1) klM_time8(3,1) klM_time9(3,1) klM_time10(3,1)],'black')
+plot(2.^(1:10), [klM_time1(3,2) klM_time2(3,2) klM_time3(3,2) klM_time4(3,2) klM_time5(3,2) klM_time6(3,2) klM_time7(3,2) klM_time8(3,2) klM_time9(3,2) klM_time10(3,2)],'black')
+plot(2.^(1:10), [meanM_time1(3) meanM_time2(3) meanM_time3(3) meanM_time4(3) meanM_time5(3) meanM_time6(3) meanM_time7(3) meanM_time8(3) meanM_time9(3) meanM_time10(3)],'r')
+plot(2.^(1:10), [lmpM_time1(3) lmpM_time2(3) lmpM_time3(3) lmpM_time4(3) lmpM_time5(3) lmpM_time6(3) lmpM_time7(3) lmpM_time8(3) lmpM_time9(3) lmpM_time10(3)],'g')
+plot(2.^(1:10), [glrM_time1(3) glrM_time2(3) glrM_time3(3) glrM_time4(3) glrM_time5(3) glrM_time6(3) glrM_time7(3) glrM_time8(3) glrM_time9(3) glrM_time10(3)],'b')
+
 subplot(2,2,4)
 grid minor
 hold on
-plot([1 2 4], klM_time4(:,1),'black')
-plot([1 2 4], klM_time4(:,2),'black')
-plot([1 2 4], meanM_time4,'r')
-plot([1 2 4], lmpM_time4,'g')
-plot([1 2 4], glrM_time4,'b')
+plot(2.^(1:10), [klM_time1(4,1) klM_time2(4,1) klM_time3(4,1) klM_time4(4,1) klM_time5(4,1) klM_time6(4,1) klM_time7(4,1) klM_time8(4,1) klM_time9(4,1) klM_time10(4,1)],'black')
+plot(2.^(1:10), [klM_time1(4,2) klM_time2(4,2) klM_time3(4,2) klM_time4(4,2) klM_time5(4,2) klM_time6(4,2) klM_time7(4,2) klM_time8(4,2) klM_time9(4,2) klM_time10(4,2)],'black')
+plot(2.^(1:10), [meanM_time1(4) meanM_time2(4) meanM_time3(4) meanM_time4(4) meanM_time5(4) meanM_time6(4) meanM_time7(4) meanM_time8(4) meanM_time9(4) meanM_time10(4)],'r')
+plot(2.^(1:10), [lmpM_time1(4) lmpM_time2(4) lmpM_time3(4) lmpM_time4(4) lmpM_time5(4) lmpM_time6(4) lmpM_time7(4) lmpM_time8(4) lmpM_time9(4) lmpM_time10(4)],'g')
+plot(2.^(1:10), [glrM_time1(4) glrM_time2(4) glrM_time3(4) glrM_time4(4) glrM_time5(4) glrM_time6(4) glrM_time7(4) glrM_time8(4) glrM_time9(4) glrM_time10(4)],'b')
