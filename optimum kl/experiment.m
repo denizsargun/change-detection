18487e5a19b39c8379608d4889524d8d7f5e09e5
denizsargun <denizsargun@gmail.com>
2019-04-11 01:53:12 -0400

    renewed figures

--- optimum kl/experiment.m
+++ optimum kl/experiment.m
@@ -30,13 +30,13 @@ classdef experiment < handle
             % fprintf(fid,pwd);
             % fclose(fid);
             
-            obj.alphabet = [-2 -1 0 1 2];
-            obj.beta = .5;
+            obj.alphabet = [-1 0 1];
+            obj.beta = 1/4;
             obj.glrThrRange = 2.^(0:.25:10)';
-            obj.it = 1e4*[0 0 0 0 1; ... % klM pfa, pd, mtbf, delay, time
-                0 0 0 0 1; ... % meanM pfa, pd, mtbf, delay, time
-                0 0 0 0 1; ... % lmpM pfa, pd, mtbf, delay, time
-                0 0 0 0 1];    % glrM pfa, pd, mtbf, delay, time
+            obj.it = 1e4*[1 1 0 0 0; ... % klM pfa, pd, mtbf, delay, time
+                1 1 0 0 0; ... % meanM pfa, pd, mtbf, delay, time
+                0 0 0 0 0; ... % lmpM pfa, pd, mtbf, delay, time
+                1 1 0 0 0];    % glrM pfa, pd, mtbf, delay, time
             obj.klMeanRange = (0:.05:max(obj.alphabet))';
             obj.klRadiusRange = 2.^(-8:1:-3)';
             obj.lmpThrRange = -2.^(0:.2:2.4)';
@@ -44,11 +44,11 @@ classdef experiment < handle
             obj.maxFunEvals = 500;
             % default MaxIter is 400
             obj.maxIter = 400;
-            obj.meanMeanRange = (obj.beta:.06:max(obj.alphabet))';
+            obj.meanMeanRange = (min(obj.alphabet):.05:max(obj.alphabet))';
             obj.methodNames = {'klM', 'meanM', 'lmpM', 'glrM'};
             obj.sampleSize = 25;
             obj.testNames = {'pfaT','pdT','mtbfT','delayT','timeT'};
-            obj.unchangedDist = 1/5*ones(5,1);
+            obj.unchangedDist = 1/3*ones(3,1);
             obj.utility = utility(obj);
             obj.utility.setup()
             obj.writer = writer(obj);
